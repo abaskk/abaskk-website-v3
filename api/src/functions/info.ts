87fb951986d02419becc62ab13e07bf6c0826d02
Amruth HB <amruthharibaskar@gmail.com>
2024-07-14 01:57:36 -0700

    typescript backend test

---
+++ api/src/functions/info.ts
@@ -0,0 +1,14 @@
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+// import { info } from "console";
+import { data } from "../tempData/data";
+
+export async function infoTrigger(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    context.log(`Http function processed request for url "${request.url}"`);
+    return JSON.parse(data);
+};
+
+app.http('info', {
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    handler: infoTrigger
+});
---
+++ api/src/tempData/data.ts
@@ -0,0 +1,109 @@
+export const data: string = `
+{
+	"name": "Amruth",
+	"year": "Third",
+	"resume_link": "",
+	"github_link": "https://github.com/abaskk",
+	"linkedin": "https://www.linkedin.com/in/amruth-baskar/",
+	"email": "amruthharibaskar@gmail.com",
+	"description": "Hi! I'm currently a third year CS student at the University of Waterloo. I'm currently looking for Fall 2023 and Summer 2024 internships. In my past co-op terms I have worked on backend, fullstack and platform development. Recently, I have been exploring low-level and systems programming.",
+	"skills": [
+		"Python",
+		"C++",
+		"SQL / NoSQL",
+		"AWS",
+		"Kubernetes",
+		"Docker",
+		"Node.js",
+		"CI / CD"
+	],
+	"projects": [
+		{
+			"title": "Personal Website",
+			"technologies": [
+				"Docker",
+				"React",
+				"Express",
+				"JWT"
+			],
+			"description": "A simple fullstack website with authentication and an admin page for me to add information on the fly. The frontend utilizes React,Tailwind and the backend is a simple Express API.",
+			"repo_link": "",
+			"demo_link": ""
+		},
+		{
+			"title": "Word Search Solver",
+			"technologies": [
+				"Python",
+				"Flask",
+				"GCP"
+			],
+			"description": "A word search solver that utilizes OCR and image pre-processing to extract the word search grid from an image. The backend service utilizes grid search algorithms to find words. Currently working on a migration of the backend to C++ for faster response times",
+			"repo_link": "https://google.ca",
+			"demo_link": "https://google.ca"
+		},
+		{
+			"title": "C++ Compiler",
+			"technologies": [
+				"C++"
+			],
+			"description": "A C++ Compiler for a subset of C that supports memory allocation, pointers and arrays.",
+			"repo_link": "https://google.ca",
+			"demo_link": "https://google.ca"
+		},
+		{
+			"title": "C++ Interpreter",
+			"technologies": [
+				"C++",
+				"Algorithms"
+			],
+			"description": "A C++ interpreter for a simple language that supports conditionals and arrays. Utilizes lexing, Pratt Parsing and tree-walking to interpret the produced AST.",
+			"repo_link": "https://google.ca",
+			"demo_link": "https://google.ca"
+		}
+	],
+	"experience": [
+		{
+			"company": "Amazon",
+			"logo_gdrive_id": "1zEEsRbbSOqVLAyJXcdd9LlkH7b3yZC76",
+			"start_date": "May 2023",
+			"end_date": "Aug 2023",
+			"title": "Software Engineering Intern",
+			"team": "S2I",
+			"technologies_used": [
+				"Java",
+				"Python",
+				"AWS"
+			]
+		},
+		{
+			"company": "Vidyard",
+			"logo_gdrive_id": "13o9akGB02d3Ju32i8czzkTdMNdL8BymX",
+			"start_date": "May 2022",
+			"end_date": "Aug 2022",
+			"title": "Software Engineering Intern",
+			"team": "Platform Team",
+			"technologies_used": [
+				"Ruby on Rails",
+				"Docker",
+				"AWS",
+				"Kubernetes",
+				"GraphQL"
+			]
+		},
+		{
+			"company": "Gradient Ascent",
+			"logo_gdrive_id": "1UxuOIdPpuny7TAvVznIDlcpw1Nh_8PwH",
+			"start_date": "May 2021",
+			"end_date": "Aug 2021",
+			"title": "Software Engineering Intern",
+			"team": "Startup",
+			"technologies_used": [
+				"Ruby on Rails",
+				"Python",
+				"Flask",
+				"PyTorch",
+				"GCP"
+			]
+		}
+	]
+}`;
