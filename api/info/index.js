eccc6b30a34b0b33793c4a2fcad1ade325d94758
Amruth HB <amruthharibaskar@gmail.com>
2024-07-14 00:55:13 -0700

    simplest api resp

--- api/info/index.js
+++ api/info/index.js
@@ -1,5 +1,6 @@
 import { data } from './data';
 
 module.exports = async function (context, req) {
-    context.res(JSON.parse(data));
+    //context.res(JSON.parse(data));
+    context.res.status(200).send("Hello World");
 };
