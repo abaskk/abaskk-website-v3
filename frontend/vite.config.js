4215ec343aca465c3100454c06c0533f84a14fba
Amruth Baskar <amruthharibaskar@gmail.com>
2024-08-16 23:31:17 -0700

    client side routing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 8080
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        routes: './public/routes.json'
+      }
+    }
   }
 })
