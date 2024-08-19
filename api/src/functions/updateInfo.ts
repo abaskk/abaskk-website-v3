5649b6947d9c31c13eca2d30b4d797f46eaf4586
Amruth Baskar <amruthharibaskar@gmail.com>
2024-08-18 21:57:48 -0700

    try catch debug

--- api/src/functions/updateInfo.ts
+++ api/src/functions/updateInfo.ts
@@ -6,33 +6,42 @@ import axios from "axios";
 import { BlobServiceClient } from "@azure/storage-blob";
 
 export async function updateDataTrigger(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-
-
     let requestBody = await request.json() as UpdateInfoRequestBody;
+    
+    let updateRes: UpdateInfoResponseBody = {
+        result: false,
+        message: "unauthorized access"
+    }
+    try{
     const authMiddleWare = axios.create();
     const requestUrl = request.url;
     const authUrl = requestUrl.replace('/modify-data', '/has-permission');
     const authResult = await authMiddleWare.post(`${authUrl}`, {token: requestBody.token});
     const responseData: PermissionsResponseBody = authResult.data;
 
-    let updateRes: UpdateInfoResponseBody = {
-        result: false,
-        message: "unauthorized access"
-    }
     if (!responseData.authenticated){
         return { jsonBody: updateRes};
     }
+    }catch (error){
+        return { jsonBody: "auth part failed"};
+    }
 
-    const connectString: string = process.env.BLOB_CONNECTION_STRING!;
-    const blobServiceClient = BlobServiceClient.fromConnectionString(connectString);
-    const containerClient = blobServiceClient.getContainerClient("data");
-    const blockBlobClient = containerClient.getBlockBlobClient("info.json");
+    try{
+        const connectString: string = process.env.BLOB_CONNECTION_STRING!;
+        const blobServiceClient = BlobServiceClient.fromConnectionString(connectString);
+        const containerClient = blobServiceClient.getContainerClient("data");
+        const blockBlobClient = containerClient.getBlockBlobClient("info.json");
+    
+        // Upload data to the blob
+        await blockBlobClient.upload(requestBody.newJson, Buffer.byteLength(requestBody.newJson));
+        updateRes.result = true;
+        updateRes.message = "success!"
+        return { jsonBody: updateRes };
+    }catch(error){
+        updateRes.message = "Blob access failed"
+        return { jsonBody: updateRes };
+    }
 
-    // Upload data to the blob
-    await blockBlobClient.upload(requestBody.newJson, Buffer.byteLength(requestBody.newJson));
-    updateRes.result = true;
-    updateRes.message = "success!"
-    return { jsonBody: updateRes };
 
 };
 
