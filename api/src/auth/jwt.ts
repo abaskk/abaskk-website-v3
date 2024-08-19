d671a6c4b86e641a658dcf313a2a1f3c1c045969
Amruth Baskar <amruthharibaskar@gmail.com>
2024-08-18 18:18:30 -0700

    fix verification format

--- api/src/auth/jwt.ts
+++ api/src/auth/jwt.ts
@@ -3,7 +3,7 @@ import { decode } from 'punycode';
 
 export const jwtGenerateAccessToken = () => {
     const userName: string = process.env.JWT_USERNAME;
-    return jwt.sign({ userName }, process.env.JWT_SECRET, { expiresIn: '6h' })
+    return jwt.sign({ 'username': userName }, process.env.JWT_SECRET, { expiresIn: '6h' })
 }
 
 
